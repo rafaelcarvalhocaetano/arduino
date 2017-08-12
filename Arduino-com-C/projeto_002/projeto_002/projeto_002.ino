2ee5ffce682dca29c0c3ef638daf9f62b25a96ae
Rafael <rapha.pse@outlook.com>
2017-08-11 22:16:44 -0300

    Adicioando projeto 002

--- Arduino-com-C/projeto_002/projeto_002/projeto_002.ino
+++ Arduino-com-C/projeto_002/projeto_002/projeto_002.ino
@@ -1,19 +1,29 @@
+  int intervaloPisca = 100;
+  int intervalo2Pisca = 200;
+  int porta1 = 10;
+  int porta2 = 9;
+  
 void setup() {
-  pinMode(10, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(porta1, OUTPUT);
+  pinMode(porta2, OUTPUT);
 
 }
 
 void loop() {
+  /*
   int intervaloPisca = 100;
+  int intervalo2Pisca = 200;
+  int porta1 = 10;
+  int porta2 = 9;
+  */
   
-  digitalWrite(10, HIGH);
-  digitalWrite(9, LOW);
+  digitalWrite(porta1, HIGH);
+  digitalWrite(porta2, LOW);
   delay(intervaloPisca);
 
-  digitalWrite(10, LOW);
-  digitalWrite(9, HIGH);
-  delay(intervaloPisca);
+  digitalWrite(porta1, LOW);
+  digitalWrite(porta2, HIGH);
+  delay(intervalo2Pisca);
 
 
 }
