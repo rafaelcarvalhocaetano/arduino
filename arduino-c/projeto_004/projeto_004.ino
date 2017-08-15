2b07a4357cf7ae7aa53fefe9f79752f97801c1b0
Rafael <rapha.pse@outlook.com>
2017-08-15 10:46:16 -0300

    Ligando LED com PUSH BUTTON

--- arduino-c/projeto_004/projeto_004.ino
+++ arduino-c/projeto_004/projeto_004.ino
@@ -2,14 +2,28 @@
 Ligando LED com PUSH BUTTON
 Autor: Rafael Carvalho
 */
+int pinLed = 13;
+int pinoBtn = 2;
+int estadoBtn = 0;
 
 
 void setup() {
-
+  pinMode(pinLed, OUTPUT);
+  pinMode(pinoBtn, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  estadoBtn = digitalRead(pinoBtn);
+
+  if(estadoBtn == HIGH){
+   
+      digitalWrite(pinLed, HIGH);
+      digitalWrite(pinLed, LOW);
+   
+  }else{
+   
+    digitalWrite(pinLed, LOW);
+  }
 }
