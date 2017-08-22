67a2a6a1fc55a2b22ec3806eef455a4ca1134ed1
Rafael <rapha.pse@outlook.com>
2017-08-22 18:37:21 -0300

    Adicionando projeto java-arduino do zero - onde a interface atribui valores e envia para o arduino

---
+++ arduino-java/LED/src/led/LED.java
@@ -0,0 +1,18 @@
+
+package led;
+
+/**
+ *
+ * @author Developer
+ */
+public class LED {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Arduino ard = new Arduino();
+       ard.setVisible(true);
+    }
+    
+}
