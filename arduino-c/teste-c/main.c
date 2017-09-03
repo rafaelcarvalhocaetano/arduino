2dfb039efcc674a0b9118bb0b191991303eee449
Rafael <rapha.pse@outlook.com>
2017-09-03 14:47:47 -0300

    Conectando e Buscando dados no mysql

--- arduino-c/teste-c/main.c
+++ arduino-c/teste-c/main.c
@@ -6,21 +6,34 @@ MYSQL *con;
 MYSQL_RES *res;
 MYSQL_ROW row;
 
-char *server = "localhost";
-char *user = "root";
-char *pass = "q1w2e3r4";
-char *db = "conectandoc";
+static char *server = "localhost";
+static char *user = "root";
+static char *pass = "q1w2e3r4";
+static char *db = "conectandoc";
 
 int main()
 {
     //CONECTANDO AO BANCO
     con = mysql_init(NULL);
     if(!mysql_real_connect(con, server, user, pass, db, 0, NULL, 0)){
-        fprintf(stderr, "%s\n", mysql_error());
+        fprintf(stderr, "%s\n", mysql_error(con));
     }
     //COMANDO
     if(mysql_query(con, "show DB")){
         fprintf(stderr, "%s\n", mysql_error(con));
         exit(1);
     }
+    res = mysql_use_result(con);
+    printf("CONECTADO COM MYSQL...");
+
+    while((row = mysql_fetch_row(res)) != NULL)
+    {
+        printf("%s \n", row[0]);
+
+    }
+    mysql_free_result(res);
+    mysql_close(con);
+
+    system("pause");
+    return 0;
 }
